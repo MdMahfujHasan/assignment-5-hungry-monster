243291993a3bc5f3285f1b99d9f1afe9e856d8dd
Md. Mahfuj Hasan <mahfuj170142@diit.edu.bd>
2021-03-01 07:56:03 +0600

    edited

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
 .then(data => mealDetails(data.categories))
 
 const mealDetails = mealName => {
-    const ul = document.getElementById('meal-name');
+    const mealDiv = document.getElementById('meal-name');
     for (let i = 0; i < mealName.length; i++) {
         const allMeals = mealName[i];
+
+        const myMeal = document.createElement('div');
+
         const mealId = allMeals.idCategory;
-        const li = document.createElement('li');
-        li.innerText = mealId;
-        ul.appendChild(li);
+        const mealCategory = allMeals.strCategory;
+
+        // const h6 = document.createElement('h6');
+        // h6.innerText = mealId;
+        // const h3 = document.createElement('h3');
+        // h3.innerText = mealCategory;
+
+        // myMeal.appendChild(h6);
+        // myMeal.appendChild(h3);
+        // mealDiv.appendChild(myMeal);
+
+        const mealInfo = `
+        <h6>${mealId}</h6>
+        <h3>${mealCategory}</h3>
+        `
+        myMeal.innerHTML = mealInfo;
+        mealDiv.appendChild(myMeal);
     }
 }
