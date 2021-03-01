8c85ae7f6fbed1fe2faa790b0506f15fd64a65c8
Md. Mahfuj Hasan <mahfuj170142@diit.edu.bd>
2021-03-02 02:13:53 +0600

    not finished

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => mealDetails(data.categories))
+    .then(res => res.json())
+    .then(data => mealDetails(data.categories))
 
 const mealDetails = mealName => {
     const mealDiv = document.getElementById('meal-name');
@@ -15,15 +15,30 @@ const mealDetails = mealName => {
         const mealDescription = allMeals.strCategoryDescription;
 
         const mealInfo = `
-        <img src="${mealImg}">
-        <h4>${mealId}</h4>
-        <h3>${mealCategory}</h3>
+        <img onclick="details()" src="${mealImg}">
+        <h4 onclick="details()">${mealId}</h4>
+        <h3 onclick="details()">${mealCategory}</h3>
         <p id="meal-description">${mealDescription}</p>
         `
         myMeal.innerHTML = mealInfo;
         mealDiv.appendChild(myMeal);
     }
 }
-function my(){
-   document.getElementById('meal-description').style.display = "block";
+
+function details(){
+    document.getElementById('meal-description').style.display = "block";
 }
+
+function getInputValue(){
+    const inputValue = document.getElementById('input').value;
+    alert(inputValue);
+}
+
+// const h6 = document.createElement('h6');
+        // h6.innerText = mealId;
+        // const h3 = document.createElement('h3');
+        // h3.innerText = mealCategory;
+
+        // myMeal.appendChild(h6);
+        // myMeal.appendChild(h3);
+        // mealDiv.appendChild(myMeal);
